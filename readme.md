985ac91ee0e9f4b6dddfd5783a667ee4063560aa
Isak La Fleur <isak.engdahl@gmail.com>
2017-11-14 14:41:11 +0100

    small update in the readme file.

--- readme.md
+++ readme.md
@@ -3,3 +3,18 @@ Using the JSON data from  [JSONPlaceholder](https://jsonplaceholder.typicode.com
 
 # Explaining how to setup a own API server in Express
 
+
+Import the whole country list by running
+
+`$ node ./bin/seed.js`
+
+
+ROUTES:
+
+// makes api calls from a remote server with help of Axios:
+[http://localhost:3000/posts/](http://localhost:3000/posts/) // list all posts
+[http://localhost:3000/posts/1](http://localhost:3000/posts/1) // list one unique post by id
+
+// Serve json data with help of express local
+[http://localhost:3000/api/countries](http://localhost:3000/api/countries) // list all countries in the database
+[http://localhost:3000/api/countries/SE](http://localhost:3000/api/countries/SE) // show one unique country by country code
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var Country = require("../models/Country").Country;
 
-router.get("/", (req, res) => {
+router.get("/countries", (req, res) => {
   Country.find({}, (err, result) => {
     if (err) {
       res.status(500).json(err);
@@ -10,7 +10,21 @@ router.get("/", (req, res) => {
       const data = {
         countries: result
       };
-      res.json(data);
+      res.status(200).json(data);
+    }
+  });
+});
+
+router.get("/countries/:countryCode", (req, res) => {
+  const cc = req.params.countryCode;
+  Country.find({ code: cc }, (err, result) => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      const data = {
+        countries: result
+      };
+      res.status(200).json(data);
     }
   });
 });
